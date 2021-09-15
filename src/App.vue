66da87e4c84d8ce6e8cf09f83c223b615f4a7fd2
Ashley D <webdevmvp@gmail.com>
2021-09-15 23:03:19 +0000

    adds files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mx-auto p-6 bg-gray-200">
+    <simple-list />
+  </div>
+</template>
+
+<script>
+import SimpleList from "@/components/SimpleList";
+import CardList from "@/components/CardList";
+
+export default {
+  components: {
+    SimpleList,
+    CardList
+  }
+};
+</script>
